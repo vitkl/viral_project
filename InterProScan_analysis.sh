610b7657b479d2484a852f6a7c726bc0556d28b5
vitkl <klv2706w@gmail.com>
2017-06-29 20:22:04 +0100

    InterProScan result in GFF3 + domains to network draft

--- InterProScan_analysis.sh
+++ InterProScan_analysis.sh
@@ -1,8 +1,10 @@
 ssh vitalii@ebi-cli
-bsub rsync vitalii@petsalakigroup02-ml.windows.ebi.ac.uk:~/Desktop/vitalii /nfs/research/petsalaki/users/vitalii -r -av
+bsub -Is $SHELL
+rsync vitalii@petsalakigroup02-ml.windows.ebi.ac.uk:~/Desktop/vitalii /nfs/research/petsalaki/users/vitalii -r -av --delete
+
 ## with match lookup 
-bsub rsync /nfs/research/petsalaki/users/vitalii /hps/nobackup/research/petsalaki/users/ -r -av
-bsub -n 16 -q research-rh7 -M 16384 -R "rusage[mem=16384]" /hps/nobackup/research/petsalaki/users/vitalii/my_interproscan/interproscan-5.24-63.0/interproscan.sh -i /hps/nobackup/research/petsalaki/users/vitalii/vitalii/viral_project/data_files/all_human_viral_proteins.fasta -f tsv -iprlookup -goterms -b /hps/nobackup/research/petsalaki/users/vitalii/vitalii/viral_project/processed_data_files/all_human_viral_protein_domains.tsv
+bsub rsync /nfs/research/petsalaki/users/vitalii /hps/nobackup/research/petsalaki/users/ -r -av --delete
+bsub -n 16 -q research-rh7 -M 12288 -R "rusage[mem=12288]" /hps/nobackup/research/petsalaki/users/vitalii/my_interproscan/interproscan-5.24-63.0/interproscan.sh -i /hps/nobackup/research/petsalaki/users/vitalii/vitalii/viral_project/data_files/all_human_viral_proteins.fasta -f gff3 -iprlookup -goterms -b /hps/nobackup/research/petsalaki/users/vitalii/vitalii/viral_project/processed_data_files/all_human_viral_protein_domains
 bsub rsync /hps/nobackup/research/petsalaki/users/vitalii /nfs/research/petsalaki/users/ -r -av
 
 bsub -Is $SHELL 
@@ -10,5 +12,5 @@ rsync /nfs/research/petsalaki/users/vitalii/vitalii vitalii@petsalakigroup02-ml.
 exit
 
 ## without match lookup 
-bsub -n 16 -q research-rh7 -M 16384 -R "rusage[mem=16384]" /hps/nobackup/research/petsalaki/users/vitalii/my_interproscan/interproscan-5.24-63.0/interproscan.sh -i /hps/nobackup/research/petsalaki/users/vitalii/vitalii/viral_project/data_files/all_human_viral_proteins.fasta -dp -f tsv -iprlookup -goterms -b /hps/nobackup/research/petsalaki/users/vitalii/vitalii/viral_project/processed_data_files/all_human_viral_protein_domains_nolookup.tsv
-bsub rsync /hps/nobackup/research/petsalaki/users/vitalii /nfs/research/petsalaki/users/ -r -av
+# bsub -n 16 -q research-rh7 -M 16384 -R "rusage[mem=16384]" /hps/nobackup/research/petsalaki/users/vitalii/my_interproscan/interproscan-5.24-63.0/interproscan.sh -i /hps/nobackup/research/petsalaki/users/vitalii/vitalii/viral_project/data_files/all_human_viral_proteins.fasta -dp -f tsv -iprlookup -goterms -b /hps/nobackup/research/petsalaki/users/vitalii/vitalii/viral_project/processed_data_files/all_human_viral_protein_domains_nolookup.tsv
+# bsub rsync /hps/nobackup/research/petsalaki/users/vitalii /nfs/research/petsalaki/users/ -r -av
