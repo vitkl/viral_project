35c3f888deeabbbcb665a26371d4c2bc14e2da89
vitkl <klv2706w@gmail.com>
2017-07-28 16:19:02 +0100

    domain permutation v3 including rare domains

---
+++ knit_domain_enrichment_permutation_on_cluster.R
@@ -0,0 +1,9 @@
+# knit RMD on cluster
+library(rmarkdown)
+rmarkdown::render(input = "/hps/nobackup/research/petsalaki/users/vitalii/vitalii/viral_project/domain_enrichment_permutation.Rmd", 
+                  output_format = "html_document", 
+                  output_file="/hps/nobackup/research/petsalaki/users/vitalii/vitalii/viral_project/domain_enrichment_permutation_clust.html")
+
+# bsub -n 16 -q research-rh7 -M 16000 -R "rusage[mem=16000]" Rscript /hps/nobackup/research/petsalaki/users/vitalii/vitalii/viral_project/knit_domain_enrichment_permutation_on_cluster.R
+# bsub -n 16 -q research-rh7 -M 12288 -R "rusage[mem=12288]" Rscript /hps/nobackup/research/petsalaki/users/vitalii/vitalii/viral_project/knit_domain_enrichment_permutation_on_cluster.R
+# bsub -n 16 -q research-rh7 -M 20000 -R "rusage[mem=20000]" Rscript /hps/nobackup/research/petsalaki/users/vitalii/vitalii/viral_project/knit_domain_enrichment_permutation_on_cluster.R
