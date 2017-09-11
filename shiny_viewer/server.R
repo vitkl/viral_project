97160f47a848a9067edc03be3932ae2b373dac42
vitkl <klv2706w@gmail.com>
2017-09-11 13:30:22 +0100

    shiny viewer for what_we_find_VS_ELM

--- shiny_viewer/server.R
+++ shiny_viewer/server.R
@@ -1,4 +1,10 @@
 library(shiny)
+library(MItools)
+library(rtracklayer)
+library(ggplot2)
+library(GGally)
+library(RColorBrewer)
+library(R.utils)
 
 # Define server logic required to simulate gene expression and plot heatmap
 shinyServer(function(input, output, session) {
@@ -7,19 +13,44 @@ shinyServer(function(input, output, session) {
     missing_packages = packages[!packages %in% names(installed.packages()[,"Package"])]
     if(length(missing_packages) > 0) stop(paste0("required packages are missing: ", paste(missing_packages, collapse = " ")))
     
-    library(MItools)
-    library(rtracklayer)
-    library(ggplot2)
-    library(GGally)
-    library(RColorBrewer)
-    library(R.utils)
-    
-    if(grepl("gz",input$path)) {
-        path.gz = input$path
-        gunzip(path, overwrite = T, remove = F)
-        path = gsub("\\.gz$","", path.gz)
+    observe({
+        if(grepl("gz",input$path)) {
+            stop("gunzip *.RData first")
         } else path = input$path
-    load(path)
+        load(path)
+        enrich_plot_set_choices = grep("enrichment",ls(), value = T)
+        which_matrix = sapply(enrich_plot_set_choices, function(x) class(eval(parse(text = x)))) == "matrix"
+        enrich_plot_set_choices = enrich_plot_set_choices[which_matrix]
+        updateSelectInput(session, inputId = "enrich_plot_set", 
+                          choices = enrich_plot_set_choices,
+                          selected = enrich_plot_set_choices)
+        
+        which_EmpiricalPval = sapply(ls(), function(x) class(eval(parse(text = x)))) == "XYZinteration_XZEmpiricalPval"
+        for_2d_bin = names(which_EmpiricalPval)[which_EmpiricalPval]
+        updateRadioButtons(session, inputId = "bin2d_pval_plot_set", choices = for_2d_bin,
+                           selected = for_2d_bin[1])
+    })
     
+    
+    
+    output$bin2d_plot <- renderPlot({
+        PermutResult2D(res = eval(parse(text = input$bin2d_pval_plot_set)), N = input$N_pairs) +
+            ggtitle(input$bin2d_plotname)
+    })
+    
+    output$pval_plot <- renderPlot({
+        plot(eval(parse(text = input$bin2d_pval_plot_set)), main = input$pval_plotname)
+    })
+    
+    output$enrich_plot <- renderPlot({
+        load(input$path)
+        rm(plotEnrichment)
+        results2plot = list()
+        for (i in 1:(length(input$enrich_plot_set))) {
+            results2plot = c(results2plot, list(eval(parse(text = input$enrich_plot_set[i]))))
+        }
+        plotEnrichment(list = results2plot,
+                       random_domains = enrichmentRANDOM, 
+                       domains_known_mapped = domains_known_mapped, type = input$enrich_plot_type, plot_type = "l")
+    })
 })
-    
--- shiny_viewer/ui.R
+++ shiny_viewer/ui.R
@@ -8,18 +8,18 @@ shinyUI(pageWithSidebar(
     
     # Sidebar with a slider input for number of observations
     sidebarPanel(
-        textInput(inputId = "path", label = "path to *.RData file containing the results", value = "/Users/vitalii/Desktop/vitalii/viral_project/processed_data_files/what_we_find_VS_ELM_output_freq.RData"),
+        textInput(inputId = "path", label = "path to *.RData file containing the results", value = "../processed_data_files/what_we_find_VS_ELM_clust.RData"),
         selectInput(inputId = "enrich_plot_set", label = "Our prediction vs ELM domains: which results to compare?",
-                    choices = "unavailable", multiple = T),
+                    choices = "enrichment", selected = "enrichment", multiple = T),
         radioButtons(inputId = "enrich_plot_type", label = "Our prediction vs ELM domains: plot type",
                      choices = list(pval = "pval", odds_ratio = "odds_ratio", count = "count"), selected = "count"),
-        radioButtons(inputId = "bin2d_plot_set", label = "Characteristics of top protein-domain pairs: which results to look at?",
+        radioButtons(inputId = "bin2d_pval_plot_set", label = "Characteristics of top protein-domain pairs and pvalue distribution: which results to look at?",
                      choices = "unavailable"),
+        textInput(inputId = "bin2d_plotname", label = "Characteristics of top protein-domain pairs: plot name", value = ""),
         sliderInput(inputId = "N_pairs", label = "Characteristics of top protein-domain pairs: how many top pairs to choose",
                     min = 10, max = 5000, step = 10,
                     value = 250),
-        radioButtons(inputId = "pval_plot_set", label = "Empirical pvalue plot: which results to look at?",
-                     choices = "unavailable")
+        textInput(inputId = "pval_plotname", label = "Empirical pvalue plot: plot name", value = "")
         ),
     
     # Show a plot of the generated distribution
@@ -27,13 +27,13 @@ shinyUI(pageWithSidebar(
         # plot heatmap
         tabsetPanel(
             tabPanel(title = "Our prediction vs ELM domains",
-                     plotOutput("enrich_plot", height = "auto")
+                     plotOutput("enrich_plot", height = "800px")
             ),
-            tabPanel(title = "Characteristics of top protein-domain pairs:",
-                     plotOutput("bin2d_plot", height = "auto")
+            tabPanel(title = "Characteristics of top protein-domain pairs",
+                     plotOutput("bin2d_plot", height = "900px")
             ),
             tabPanel(title = "Empirical pvalue plot",
-                     plotOutput("pval_plot", height = "auto")
+                     plotOutput("pval_plot", height = "600px")
         )
     )
     
